e25097d7e3cec6ae3eb9dbd8371420d635357096
JerreeLynn <jerreelynn@gmail.com>
2020-02-15 16:36:19 -0500

    Add unit class that will encompass all living entities, including heroes and enemies

--- core/src/com/mygdx/entities/Hero.java
+++ core/src/com/mygdx/entities/Hero.java
@@ -3,8 +3,7 @@ package com.mygdx.entities;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
-public class Hero extends Entity{
-
+public class Hero extends Unit{
 	public Hero(int col, int row, Sprite sprite) {
 		super(col, row, sprite);
 	}
---
+++ core/src/com/mygdx/entities/Unit.java
@@ -0,0 +1,11 @@
+package com.mygdx.entities;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public abstract class Unit extends Entity{
+
+	public Unit(int col, int row, Sprite sprite) {
+		super(col, row, sprite);
+	}
+
+}
