01c05fd80a8b2e54a3447f1ec28d3db004595b09
JerreeLynn <jerreelynn@gmail.com>
2020-02-17 21:14:31 -0500

    empty status class files so i dont forget

---
+++ core/src/com/mygdx/statuses/StatusEffect.java
@@ -0,0 +1,23 @@
+package com.mygdx.statuses;
+
+public class StatusEffect {
+	public int strMod = 0;
+	public int arcMod = 0;
+	public int conMod = 0;
+	public int finMod = 0;
+	public int pDmgMod = 0;
+	public int mDmgMod = 0;
+	public int HPMod = 0;
+	public int iniMod = 0;
+	public int cdrMod = 0;
+	public int pResMod = 0;
+	public int mResMod = 0;
+	public int moveMod = 0;
+	public int dodgeMod = 0;
+	public int pRedMod = 0;
+	public int mRedMod = 0;
+	public int swiftMod = 0;
+	public int accMod = 0;
+	public StatusEffect() {
+	}
+}
